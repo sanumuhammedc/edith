b945c9a4dbbab4bea20c709b67ee54a285caca24
sunith vs <sunithvazhenkada@gmail.com>
2021-07-29 23:48:30 +0530

    Added hero section and rules

--- edith website/js/scripts.js
+++ edith website/js/scripts.js
@@ -11,7 +11,7 @@
       // Countdown
     	// To change date, simply edit: var endDate = January 26, 2016 20:39:00";
     	$(function() {
-    	  var endDate = "January 26, 2016 20:39:00";
+    	  var endDate = "August 07, 2021 10:00:00";
     	  $('.tk-countdown .row').countdown({
     		date: endDate,
     		render: function(data) {
